1be7bff0353e4aaf89532269479086b3ed6f3932
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2019-08-10 10:45:43 +0200

    Add coach

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,21 +1,45 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
-export class Home extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Home</Text>
-      </View>
-    );
-  }
+export function Home() {
+  const [coachAdvice, setCoachAdvice] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => setCoachAdvice(1)}>
+        <Text style={styles.coachIcon}>👨‍🏫</Text>
+      </TouchableOpacity>
+      {coachAdvice === 1 && (
+        <TouchableOpacity onPress={() => setCoachAdvice(2)}>
+          <Text style={styles.advice}>
+            Bonjour bonjour c'est moi le coach !
+          </Text>
+        </TouchableOpacity>
+      )}
+      {coachAdvice === 2 && (
+        <TouchableOpacity onPress={() => setCoachAdvice(0)}>
+          <Text style={styles.advice}>Hey ! C'est encore moi</Text>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  coachIcon: {
+    fontSize: 75,
+  },
+  advice: {
+    borderWidth: 2,
+    borderColor: 'gainsboro',
+    borderRadius: 5,
+    height: 50,
+    marginBottom: 10,
+    padding: 10,
   },
 });
