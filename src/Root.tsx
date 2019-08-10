112facd39909f4551275c79b17c78d59a66bdced
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2019-08-10 09:47:16 +0200

    Add icons to tab bar

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,37 @@
+import React from 'react';
+import { AntDesign } from '@expo/vector-icons/';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 import { Home } from './Home';
 import { Discover } from './Discover';
 import { Community } from './Community';
 import { Profile } from './Profile';
 
-const TabNavigator = createBottomTabNavigator({
-  Home: Home,
-  Découvrir: Discover,
-  Communauté: Community,
-  Profil: Profile,
-});
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: Home,
+    Découvrir: Discover,
+    Communauté: Community,
+    Profil: Profile,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = 'home';
+        } else if (routeName === 'Découvrir') {
+          iconName = `find`;
+        } else if (routeName === 'Communauté') {
+          iconName = `earth`;
+        } else if (routeName === 'Profil') {
+          iconName = `user`;
+        }
+
+        return <AntDesign name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+  },
+);
 
 export default createAppContainer(TabNavigator);
