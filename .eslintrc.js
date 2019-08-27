584e9acf3835b9da3abf59dc02774f6270e09e7a
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2019-08-27 17:55:50 +0200

    Clean ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,6 @@
 module.exports = {
-  env: {
-    es6: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: ['react-hooks'],
+  extends: 'react-app',
   rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/explicit-function-return-type': [
-      'warn',
-      { allowExpressions: true, allowTypedFunctionExpressions: true },
-    ],
-    '@typescript-eslint/no-use-before-define': ['error', { variables: false }],
-    'react/display-name': 'off',
+    'jsx-a11y/accessible-emoji': 'off',
   },
 };
