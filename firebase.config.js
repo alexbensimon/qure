62f2344de00d0c17f46eac96068e2aad680a81e9
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2019-09-17 07:46:48 +0200

    Config

--- firebase.config.js
+++ firebase.config.js
@@ -2,8 +2,13 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  projectId: 'qure-8a945',
   apiKey: 'AIzaSyDA8rY7oa3IUkux9etOXMSaFb5YY0SP6HY',
+  authDomain: 'qure-8a945.firebaseapp.com',
+  databaseURL: 'https://qure-8a945.firebaseio.com',
+  projectId: 'qure-8a945',
+  storageBucket: 'qure-8a945.appspot.com',
+  messagingSenderId: '1011253080990',
+  appId: '1:1011253080990:web:d67f8e2e15eb6cfe1e9c8f',
 };
 
 firebase.initializeApp(firebaseConfig);
