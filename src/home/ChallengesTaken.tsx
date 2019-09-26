49eadd56b57505d5ba0c8bd342f51079fb02fef0
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2019-09-26 20:10:16 +0200

    Placeholder on home when no challenges taken

--- src/home/ChallengesTaken.tsx
+++ src/home/ChallengesTaken.tsx
@@ -1,20 +1,23 @@
 import firebase from 'firebase';
 import React, { Component } from 'react';
 import { RefreshControl, ScrollView, StyleSheet } from 'react-native';
-import { Text } from 'react-native-elements';
+import { Text, Button } from 'react-native-elements';
 import { colors } from '../colors';
 import { ChallengeTakenType } from '../globalTypes';
 import { ChallengeTaken } from './ChallengeTaken';
+import { withNavigation, NavigationScreenProps } from 'react-navigation';
+
+type Props = NavigationScreenProps;
 
 type State = {
   challengesTaken: Array<ChallengeTakenType>;
   refreshing: boolean;
 };
 
-export class ChallengesTaken extends Component<{}, State> {
+class RawChallengesTaken extends Component<Props, State> {
   state: State = {
     refreshing: false,
-    challengesTaken: [],
+    challengesTaken: null,
   };
 
   async componentDidMount() {
@@ -50,8 +53,10 @@ export class ChallengesTaken extends Component<{}, State> {
   };
 
   render() {
+    const { navigation } = this.props;
     const { challengesTaken, refreshing } = this.state;
-    return (
+
+    return challengesTaken === null ? null : (
       <ScrollView
         refreshControl={
           <RefreshControl
@@ -63,18 +68,32 @@ export class ChallengesTaken extends Component<{}, State> {
         <Text h1 style={styles.title}>
           Défis en cours
         </Text>
-        {challengesTaken.map(challengeTaken => (
-          <ChallengeTaken
-            key={challengeTaken.id}
-            challengeTaken={challengeTaken}
-            failChallenge={() => this.failChallenge(challengeTaken.id)}
-            reload={this.fetchData}
-          />
-        ))}
+        {challengesTaken.length > 0 ? (
+          challengesTaken.map(challengeTaken => (
+            <ChallengeTaken
+              key={challengeTaken.id}
+              challengeTaken={challengeTaken}
+              failChallenge={() => this.failChallenge(challengeTaken.id)}
+              reload={this.fetchData}
+            />
+          ))
+        ) : (
+          <>
+            <Button
+              title="Je veux relever un défi !"
+              onPress={() => navigation.navigate('Découvrir')}
+              buttonStyle={styles.button}
+              titleStyle={styles.buttonTitle}
+              containerStyle={styles.buttonContainer}
+            ></Button>
+          </>
+        )}
       </ScrollView>
     );
   }
 }
+
+export const ChallengesTaken = withNavigation(RawChallengesTaken);
 
 const styles = StyleSheet.create({
   title: {
@@ -82,5 +101,17 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: colors.dark,
     fontFamily: 'concert-one-regular',
+  },
+  button: {
+    backgroundColor: colors.dark,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    marginHorizontal: 10,
+  },
+  buttonTitle: {
+    fontFamily: 'concert-one-regular',
+    color: colors.primary,
+    fontSize: 32,
   },
 });
