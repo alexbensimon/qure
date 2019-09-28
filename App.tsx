cedd7952b8838bcfe08cf0aa20d44f6ace6e0462
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2019-09-28 20:05:27 +0200

    Clean warning ignore

--- App.tsx
+++ App.tsx
@@ -15,8 +15,6 @@ export default class App extends Component<{}, State> {
 
   async componentDidMount() {
     YellowBox.ignoreWarnings(['Setting a timer']);
-    // eslint-disable-next-line no-console
-    console.ignoredYellowBox = ['Setting a timer'];
     await Font.loadAsync({
       'concert-one-regular': require('./assets/fonts/ConcertOne-Regular.ttf'),
     });
