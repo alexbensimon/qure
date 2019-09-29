8b6f0db3c7215211b09b9fe3f5b8e77216b9854f
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2019-09-30 01:39:49 +0200

    Fix login

--- src/Login.tsx
+++ src/Login.tsx
@@ -5,107 +5,100 @@ import { StyleSheet, View } from 'react-native';
 import { Button, Icon, Text } from 'react-native-elements';
 import { colors } from './colors';
 
-export const Login: FC = () => {
-  async function facebookLogIn() {
-    try {
-      const { type, token } = await Facebook.logInWithReadPermissionsAsync(
-        '2808452099169831',
-        {
-          permissions: ['public_profile', 'user_friends'],
-        },
-      );
-      if (type === 'success') {
-        await firebase
-          .auth()
-          .setPersistence(firebase.auth.Auth.Persistence.LOCAL);
-        const credential = firebase.auth.FacebookAuthProvider.credential(token);
-        firebase
-          .auth()
-          .signInWithCredential(credential)
-          .catch(error => {
-            // eslint-disable-next-line no-console
-            console.log('Oups! Firebase signInWithCredential error: ', error);
-          });
-        firebase.auth().onAuthStateChanged(async user => {
-          if (user) {
+export const Login: FC = () => (
+  <>
+    <View style={styles.containerTop}>
+      <Text style={styles.title}>Qure</Text>
+    </View>
+    <View style={styles.containerBottom}>
+      <Button
+        buttonStyle={styles.button}
+        titleStyle={styles.buttonTitle}
+        title="Connexion avec Facebook"
+        icon={
+          <Icon
+            name="facebook-box"
+            type="material-community"
+            color="white"
+            size={32}
+            containerStyle={styles.iconContainer}
+          />
+        }
+        onPress={facebookLogIn}
+      ></Button>
+    </View>
+  </>
+);
+
+const facebookLogIn = async () => {
+  const userCollection = firebase.firestore().collection('users');
+  try {
+    const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+      '2808452099169831',
+      {
+        permissions: ['public_profile', 'user_friends'],
+      },
+    );
+    if (type === 'success') {
+      await firebase
+        .auth()
+        .setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+      const credential = firebase.auth.FacebookAuthProvider.credential(token);
+      const userCredential = await firebase
+        .auth()
+        .signInWithCredential(credential)
+        .catch(error => {
+          // eslint-disable-next-line no-console
+          console.log('Oups! Firebase signInWithCredential error: ', error);
+        });
+      if (userCredential && userCredential.user) {
+        const user = userCredential.user;
+        userCollection.doc(user.uid).set(
+          {
+            name: user.displayName,
+            photoUrl: user.photoURL,
+            facebookId: user.providerData[0].uid,
+            coach: 'basic',
+          },
+          { merge: true },
+        );
+        const { data: fbFriends } = await (await fetch(
+          `https://graph.facebook.com/${user.providerData[0].uid}/friends?access_token=${token}`,
+        )).json();
+        const promises = [];
+        fbFriends.forEach(friend => {
+          promises.push(
             firebase
               .firestore()
               .collection('users')
-              .doc(user.uid)
-              .set(
-                {
-                  name: user.displayName,
-                  photoUrl: user.photoURL,
-                  facebookId: user.providerData[0].uid,
-                  // FIXME: verify if user already exists and do not override existing data
-                  coach: 'basic',
-                },
-                { merge: true },
-              );
-            const { data: fbFriends } = await (await fetch(
-              `https://graph.facebook.com/${user.providerData[0].uid}/friends?access_token=${token}`,
-            )).json();
-            const promises = [];
-            fbFriends.forEach(friend => {
-              promises.push(
-                firebase
-                  .firestore()
-                  .collection('users')
-                  .where('facebookId', '==', friend.id)
-                  .limit(1)
-                  .get(),
-              );
-            });
-            const querySnapshots = await Promise.all(promises);
-            const friends = [];
-            querySnapshots.forEach(querySnapshot => {
-              querySnapshot.forEach(doc => {
-                friends.push(doc);
-              });
-            });
-            friends.forEach(friend => {
-              firebase
-                .firestore()
-                .collection(`users/${user.uid}/friends`)
-                .doc(friend.id)
-                .set(friend.data());
-            });
-          }
+              .where('facebookId', '==', friend.id)
+              .limit(1)
+              .get(),
+          );
         });
-      } else {
-        // eslint-disable-next-line no-console
-        console.log('Oups! Type not success, type: ', type);
+        const querySnapshots = await Promise.all(promises);
+        const friends = [];
+        querySnapshots.forEach(querySnapshot => {
+          querySnapshot.forEach(doc => {
+            friends.push(doc);
+          });
+        });
+        friends.forEach(friend => {
+          firebase
+            .firestore()
+            .collection(`users/${user.uid}/friends`)
+            .doc(friend.id)
+            .set(friend.data());
+        });
       }
-    } catch ({ message }) {
+    } else {
       // eslint-disable-next-line no-console
-      console.log('Oups! Facebook Login Error: ', message);
+      console.log('Oups! Type not success, type: ', type);
     }
+  } catch ({ message }) {
+    // eslint-disable-next-line no-console
+    console.log('Oups! Facebook Login Error: ', message);
   }
-
-  return (
-    <>
-      <View style={styles.containerTop}>
-        <Text style={styles.title}>Qure</Text>
-      </View>
-      <View style={styles.containerBottom}>
-        <Button
-          buttonStyle={styles.button}
-          titleStyle={styles.buttonTitle}
-          title="Connexion avec Facebook"
-          icon={
-            <Icon
-              name="facebook-box"
-              type="material-community"
-              color="white"
-              size={32}
-              containerStyle={styles.iconContainer}
-            />
-          }
-          onPress={facebookLogIn}
-        ></Button>
-      </View>
-    </>
-  );
 };
 
 const styles = StyleSheet.create({
