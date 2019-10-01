fa98d83ed1dfc3572e14a0b7cbf543c8d586b0a2
Alexandre Bensimon <alexandrebensimon1@gmail.com>
2019-10-01 22:07:25 +0200

    Set status bar color

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,6 +2,7 @@ import firebase from 'firebase';
 import React, { Component } from 'react';
 import { Login } from './Login';
 import TabNavigator from './TabNavigator';
+import { StatusBar } from 'react-native';
 
 type State = {
   loggedIn: boolean | null;
@@ -21,6 +22,11 @@ export default class Root extends Component<{}, State> {
   render() {
     const { loggedIn } = this.state;
     if (loggedIn === null) return null;
-    return loggedIn === false ? <Login /> : <TabNavigator />;
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        {loggedIn === false ? <Login /> : <TabNavigator />}
+      </>
+    );
   }
 }
